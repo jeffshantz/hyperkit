4b1873d816edbd663173be1057be5d5d913f0c98
Jeff Shantz <jeff@csd.uwo.ca>
2016-03-16 10:12:34 -0400

    Implement 'POST /certificates'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@ require "base64"
 WebMock.disable_net_connect!(:allow => 'coveralls.io')
 
 RSpec.configure do |config|
+
+	config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
   config.raise_errors_for_deprecations!
   config.before(:each) do
     Hyperkit.reset!
@@ -98,3 +101,40 @@ def use_vcr_placeholder_for(text, replacement)
     end
   end
 end
+
+def unauthenticated_client
+  cli = Hyperkit::Client.new
+  cli.client_cert = cli.client_key = nil
+  cli
+end
+
+def test_certificate
+<<-EOF
+-----BEGIN CERTIFICATE-----
+MIIEDTCCAvWgAwIBAgIJAP2x5XIwszwpMA0GCSqGSIb3DQEBCwUAMIGcMQswCQYD
+VQQGEwJDQTEQMA4GA1UECAwHT250YXJpbzEPMA0GA1UEBwwGTG9uZG9uMRwwGgYD
+VQQKDBNFeGFtcGxlIENvcnBvcmF0aW9uMRUwEwYDVQQLDAxFeGFtcGxlIFVuaXQx
+FDASBgNVBAMMC2V4YW1wbGUuY29tMR8wHQYJKoZIhvcNAQkBFhBqZWZmQGV4YW1w
+bGUuY29tMB4XDTE2MDMxNjAyMzA0NFoXDTI2MDMxNDAyMzA0NFowgZwxCzAJBgNV
+BAYTAkNBMRAwDgYDVQQIDAdPbnRhcmlvMQ8wDQYDVQQHDAZMb25kb24xHDAaBgNV
+BAoME0V4YW1wbGUgQ29ycG9yYXRpb24xFTATBgNVBAsMDEV4YW1wbGUgVW5pdDEU
+MBIGA1UEAwwLZXhhbXBsZS5jb20xHzAdBgkqhkiG9w0BCQEWEGplZmZAZXhhbXBs
+ZS5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDdaPN+xiLWDA8j
+24zMyx+2YBg+8emf/WSC7pD+8hm4TzPtTUzfJssGgrcZARqUnzvWZ5XinhgBTAg3
+JP3qB8MMfI+UVgbHMododafVAglOOrosi10hzNMsSMDws5k1jNj2iZd0tLGB0w4i
+E9oGCnpzKV9oQZqg148UQhgrZSfY5vSOQvAES7PU0/Dyox38+6qpqkHwcgGFYJVL
+mq6Y4lHoUZmR8cZNYnnr5tLEvXluhDXahlH9yLJ43nDqa6HqwgJHa8Zbd3u5McEF
+tnKA7TVN2B839pAOvbnYww5BGFVVkrDUm0f+169DPsh1qbRAeOofSKUTlj8rw/Z+
+sEUKqqj5AgMBAAGjUDBOMB0GA1UdDgQWBBSMmA1R+/zdCiiUXyNxWOGbGDT1BTAf
+BgNVHSMEGDAWgBSMmA1R+/zdCiiUXyNxWOGbGDT1BTAMBgNVHRMEBTADAQH/MA0G
+CSqGSIb3DQEBCwUAA4IBAQBN9rcdFFkK0cvb4JZY7PcHG6L9KfKvNSuEoJzSuOpi
+pAsxlqnlVz1KaiPO1GzN2ikL/Ab5oKnknTXPIPDKlhQYDoAUGUQm6s2oijz5MEO/
+C3QmbHJdfPiVXY+fuE1wuJLGIB6c+cvozfeEICoLLxXgzizgnD+wGd5M2pDDiQDt
+mJbpsG2m97ZkCsH09xpDk01u0EzxoA2oA3n7SHNO/VJXJnOdMtC5qjYo9xZYjU/s
+zX6g65tuZyzJg14j9ZtT4EDwMotewuB/BzQ35oYdjUp8VP9I7AZxilNn+HAdwYYN
+fkygJ2GGlOc7XAQOrqRlCSQJQKlDK0ZVHhF29RVuibeN
+-----END CERTIFICATE-----
+EOF
+
+end
+
