04340c351e8ec3ab8bc9618276ff4596b36a9ad6
Jeff Shantz <jeff@csd.uwo.ca>
2016-03-16 13:45:50 -0400

    Implement 'GET /images/<fingerprint>'

--- lib/hyperkit/client/images.rb
+++ lib/hyperkit/client/images.rb
@@ -18,6 +18,34 @@ module Hyperkit
         response[:metadata].map { |path| path.split('/').last }
       end
 
+      # Get information on an image
+      #
+      # @return [Hash] A hash of information about the image
+      # @example Get information about an image on images.linuxcontainers.org
+      #   Hyperkit.client.api_endpoint = "https://images.linuxcontainers.org:8443"
+      #   Hyperkit.client.image("45bcc353f629b23ce30ef4cca14d2a4990c396d85ea68905795cc7579c145123") #=> {
+      #     :properties=>{:description=>"Centos 6 (amd64) (20160314_02:16)"},
+      #     :expires_at=>1970-01-01 00:00:00 UTC,
+      #     :filename=>"centos-6-amd64-default-20160314_02:16.tar.xz",
+      #     :uploaded_at=>2016-03-15 01:20:10 UTC,
+      #     :size=>54717798,
+      #     :public=>true,
+      #     :architecture=>"x86_64",
+      #     :aliases=>[],
+      #     :created_at=>2016-03-15 01:20:10 UTC,
+      #     :fingerprint=> "45bcc353f629b23ce30ef4cca14d2a4990c396d85ea68905795cc7579c145123"
+      #   }
+      def image(fingerprint)
+        response = get image_path(fingerprint)
+        response[:metadata]
+      end
+
+      private
+
+      def image_path(fingerprint)
+        File.join(images_path, fingerprint)
+      end
+
       private
 
       def images_path
