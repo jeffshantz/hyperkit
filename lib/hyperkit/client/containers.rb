89c737d1001b226df915bd72b7c0df1a83142fac
Jeff Shantz <jeff@csd.uwo.ca>
2016-03-23 12:19:47 -0400

    Implement 'GET /containers/<name>'

--- lib/hyperkit/client/containers.rb
+++ lib/hyperkit/client/containers.rb
@@ -17,7 +17,53 @@ module Hyperkit
         response[:metadata].map { |path| path.split('/').last }
       end
 
+      # Get information on a container
+      #
+      # @param name [String] Container name
+      # @return [Sawyer::Resource] Container information
+      #
+      # @example Get information about a container
+      #   Hyperkit.client.container("test-container") #=> {
+      #     :architecture => "x86_64",
+      #       :config => {
+      #         :"volatile.base_image" => "097e75d6f7419d3a5e204d8125582f2d7bdd4ee4c35bd324513321c645f0c415",
+      #         :"volatile.eth0.hwaddr" => "00:16:3e:24:5d:7a",
+      #         :"volatile.eth0.name" => "eth0",
+      #         :"volatile.last_state.idmap" =>
+      #           "[{\"Isuid\":true,\"Isgid\":false,\"Hostid\":165536,\"Nsid\":0,\"Maprange\":65536},{\"Isuid\":false,\"Isgid\":true,\"Hostid\":165536,\"Nsid\":0,\"Maprange\":65536}]"
+			#       },
+      #       :created_at => 2016-03-18 20:55:26 UTC,
+      #       :devices => {
+			#         :root => {:path => "/", :type => "disk"}
+			#       },
+      #       :ephemeral => false,
+      #       :expanded_config => {
+      #         :"volatile.base_image" => "097e75d6f7419d3a5e204d8125582f2d7bdd4ee4c35bd324513321c645f0c415",
+      #         :"volatile.eth0.hwaddr" => "00:16:3e:24:5d:7a",
+      #         :"volatile.eth0.name" => "eth0",
+      #         :"volatile.last_state.idmap" =>
+      #           "[{\"Isuid\":true,\"Isgid\":false,\"Hostid\":165536,\"Nsid\":0,\"Maprange\":65536},{\"Isuid\":false,\"Isgid\":true,\"Hostid\":165536,\"Nsid\":0,\"Maprange\":65536}]"
+      #       },
+      #       :expanded_devices => {
+      #         :eth0 => { :nictype => "bridged", :parent => "lxcbr0", :type => "nic"},
+      #         :root => { :path => "/", :type => "disk"}
+      #       },
+      #       :name => "test-container",
+      #       :profiles => ["default"],
+      #       :stateful => false,
+      #       :status => "Stopped",
+      #       :status_code => 102
+      #   }
+      def container(name, options={})
+        response = get container_path(name)
+        response[:metadata]
+      end
+
       private
+
+      def container_path(name)
+        File.join(containers_path, name)
+      end
 
       def containers_path
         "/1.0/containers"
