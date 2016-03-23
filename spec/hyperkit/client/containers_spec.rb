7878b947d237e5d133899021489e364ab69d1b39
Jeff Shantz <jeff@csd.uwo.ca>
2016-03-23 16:20:55 -0400

    Implement 'PUT /containers/<name>/state'

--- spec/hyperkit/client/containers_spec.rb
+++ spec/hyperkit/client/containers_spec.rb
@@ -22,7 +22,7 @@ describe Hyperkit::Client::Containers do
         "/1.0/containers/test2",
         "/1.0/containers/test3",
         "/1.0/containers/test4"
-			]}.to_json
+      ]}.to_json
       stub_get("/1.0/containers").
         to_return(:status => 200, body: body, :headers => {'Content-Type' => 'application/json'})
 
@@ -32,21 +32,21 @@ describe Hyperkit::Client::Containers do
 
   end
 
-  describe ".container" do
+  describe ".container", :vcr do
 
     it "retrieves a container" do
       # TODO: Create a container
 
       container = client.container("test-container")
 
-			expect(container.name).to eq("test-container")
+      expect(container.name).to eq("test-container")
       expect(container.architecture).to eq("x86_64")
       
       # TODO: Delete the container
     end
 
     it "makes the correct API call" do
-			request = stub_get("/1.0/containers/test").
+      request = stub_get("/1.0/containers/test").
         to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
 
       client.container("test")
@@ -71,10 +71,274 @@ describe Hyperkit::Client::Containers do
     end
 
     it "makes the correct API call" do
-			request = stub_get("/1.0/containers/test/state").
+      request = stub_get("/1.0/containers/test/state").
         to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
 
       client.container_state("test")
+      assert_requested request
+    end
+
+  end
+
+  describe ".start_container", :vcr do
+
+    it  "starts a stopped container" do
+      # TODO: Create and stop a container
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Stopped")
+
+      response = client.start_container("test-container")
+      client.wait_for_operation(response.id)
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Running")
+
+      # TODO: Delete the container
+    end
+
+    it "accepts a timeout" do
+      request = stub_put("/1.0/containers/test/state").
+        with(body: hash_including({
+          action: "start",
+          timeout: 30
+        })).
+        to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+
+      client.start_container("test", timeout: 30)
+      assert_requested request
+    end
+
+    it "allows the operation to be stateful" do
+      request = stub_put("/1.0/containers/test/state").
+        with(body: hash_including({
+          action: "start",
+          stateful: true
+        })).
+        to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+
+      client.start_container("test", stateful: true)
+      assert_requested request
+    end
+
+  end
+
+
+  describe ".stop_container", :vcr do
+
+    it "stops a running container" do
+      # TODO: Create and start a container
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Running")
+
+      response = client.stop_container("test-container", force: true)
+      client.wait_for_operation(response.id)
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Stopped")
+
+      # TODO: Delete the container
+    end
+
+    it "throws an error if the container is not running" do
+      # TODO: Create and stop a container
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Stopped")
+
+      response = client.stop_container("test-container")
+      expect { client.wait_for_operation(response.id) }.to raise_error(Hyperkit::BadRequest)
+
+      # TODO: Delete the container
+    end
+
+    it "accepts a timeout" do
+      request = stub_put("/1.0/containers/test/state").
+        with(body: hash_including({
+          action: "stop",
+          timeout: 30
+        })).
+        to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+
+      client.stop_container("test", timeout: 30)
+      assert_requested request
+    end
+
+    it "allows the operation to be forced" do
+      request = stub_put("/1.0/containers/test/state").
+        with(body: hash_including({
+          action: "stop",
+          force: true
+        })).
+        to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+
+      client.stop_container("test", force: true)
+      assert_requested request
+    end
+
+    it "allows the operation to be stateful" do
+      request = stub_put("/1.0/containers/test/state").
+        with(body: hash_including({
+          action: "stop",
+          stateful: true
+        })).
+        to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+
+      client.stop_container("test", stateful: true)
+      assert_requested request
+    end
+
+  end
+
+  describe ".restart_container", :vcr do
+
+    it "restarts a running container" do
+      # TODO: Create and start container
+
+      response = client.start_container("test-container")
+      client.wait_for_operation(response.id)
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Running")
+      pid_before = state.pid
+
+      response = client.restart_container("test-container", force: true)
+      client.wait_for_operation(response.id)
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Running")
+      pid_after = state.pid
+      
+      expect(pid_after).to_not eq(pid_before)
+
+      # TODO: Delete the container
+    end
+
+    it "throws an error if the container is not running" do
+      # TODO: Create and stop a container
+
+      response = client.stop_container("test-container", force: true)
+      client.wait_for_operation(response.id)
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Stopped")
+
+      response = client.restart_container("test-container")
+      expect { client.wait_for_operation(response.id) }.to raise_error(Hyperkit::BadRequest)
+
+      # TODO: Delete the container
+    end
+
+    it "allows the operation to be forced" do
+      request = stub_put("/1.0/containers/test/state").
+        with(body: hash_including({
+          action: "restart",
+          force: true
+        })).
+        to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+
+      client.restart_container("test", force: true)
+      assert_requested request
+    end
+
+    it "accepts a timeout" do
+      request = stub_put("/1.0/containers/test/state").
+        with(body: hash_including({
+          action: "restart",
+          timeout: 30
+        })).
+        to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+
+      client.restart_container("test", timeout: 30)
+      assert_requested request
+    end
+
+  end
+
+  describe ".freeze_container", :vcr do
+
+    it "suspends a running container" do
+      # TODO: Create and start container
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Running")
+
+      response = client.freeze_container("test-container")
+      client.wait_for_operation(response.id)
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Frozen")
+
+      # TODO: Delete the container
+      
+    end
+
+    it "throws an error if the container is not running" do
+      # TODO: Create and stop a container
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Stopped")
+
+      response = client.freeze_container("test-container")
+      expect { client.wait_for_operation(response.id) }.to raise_error(Hyperkit::BadRequest)
+
+      # TODO: Delete the container
+    end
+
+    it "accepts a timeout" do
+      request = stub_put("/1.0/containers/test/state").
+        with(body: hash_including({
+          action: "freeze",
+          timeout: 30
+        })).
+        to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+
+      client.freeze_container("test", timeout: 30)
+      assert_requested request
+    end
+
+  end
+
+  describe ".unfreeze_container", :vcr do
+
+    it "resumes a frozen container" do
+      # TODO: Create and freeze container
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Frozen")
+
+      response = client.unfreeze_container("test-container")
+      client.wait_for_operation(response.id)
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Running")
+
+      # TODO: Delete the container
+      
+    end
+
+    it "throws an error if the container is not frozen" do
+      # TODO: Create and start a container
+
+      state = client.container_state("test-container")
+      expect(state.status).to eq("Running")
+
+      response = client.unfreeze_container("test-container")
+      expect { client.wait_for_operation(response.id) }.to raise_error(Hyperkit::BadRequest)
+
+      # TODO: Delete the container
+    end
+
+    it "accepts a timeout" do
+      request = stub_put("/1.0/containers/test/state").
+        with(body: hash_including({
+          action: "unfreeze",
+          timeout: 30
+        })).
+        to_return(status: 200, body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+
+      client.unfreeze_container("test", timeout: 30)
       assert_requested request
     end
 
